cde0336e13d03be2aa0ece87ad0389682ff0d6ca
ASC Staff <sysadmin@asylumseekerscentre.org.au>
2017-10-20 17:24:51 +1100

    import first working status

---
+++ CRM/Groupreport/Form/Report/ContactSummaryGroup.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Groupreport_Form_Report_ContactSummaryGroup',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'ContactSummaryGroup',
+      'description' => 'ContactSummaryGroup (au.org.asylumseekerscentre.groupreport)',
+      'class_name' => 'CRM_Groupreport_Form_Report_ContactSummaryGroup',
+      'report_url' => 'au.org.asylumseekerscentre.groupreport/contactsummarygroup',
+      'component' => '',
+    ),
+  ),
+);
